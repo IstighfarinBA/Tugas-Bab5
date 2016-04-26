957d418d26735290ad45ad7a646d86e09242fceb
istighfarinba <bafarcorp@gmail.com>
2016-04-26 21:15:50 +0700

    Pembuatan main class(MainHitung)

---
+++ src/MainHitung.java
@@ -0,0 +1,13 @@
+
+public class MainHitung {
+
+    /**
+     * @NAMA : ISTIGHFARIN BAHTIAR AMRY
+     * @NIM  : 155150401111129
+     * @KELAS: SI-H
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
