f71ea51e3da446bc22f6bcd670130ed0bce697b7
istighfarinba <bafarcorp@gmail.com>
2016-04-27 01:07:35 +0700

    pembuatan MainClass(TokoBaju.java)

---
+++ src/Program Kedua/TokoBaju.java
@@ -0,0 +1,17 @@
+package TokoBaju;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @NAMA :ISTIGHFARIN BAHTIAR AMRY
+ * @KELAS :SI-H
+ * @NIM :155150401111129
+ */
+public class TokoBaju {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
