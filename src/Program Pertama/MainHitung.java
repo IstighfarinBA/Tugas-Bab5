d3ee7017e3ad8a5dbfa3794912a3ff7e04ad173d
istighfarinba <bafarcorp@gmail.com>
2016-04-27 10:24:44 +0700

    pembuatan method case1(static)

--- src/Program Pertama/MainHitung.java
+++ src/Program Pertama/MainHitung.java
@@ -1,13 +1,28 @@
 
+import java.util.Scanner;
+
 public class MainHitung {
 
     /**
      * @NAMA : ISTIGHFARIN BAHTIAR AMRY
-     * @NIM  : 155150401111129
+     * @NIM : 155150401111129
      * @KELAS: SI-H
      */
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        MainHitung a = new MainHitung();
+    }
+
+    public static void case1() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\t===PROGRAM PENJUMLAHAN===");
+        System.out.println("");
+        System.out.print("Masukkan Angka Pertama :");
+        int pertama = in.nextInt();
+        System.out.print("Masukkan Angka Kedua   :");
+        int kedua = in.nextInt();
+        //pemanggilan method static penjumlahan dengan nama class
+        hitung.penjumlahan(pertama, kedua);
 
     }
-    
 }
