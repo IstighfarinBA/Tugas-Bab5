53191263d43751a296bbf8592aba0620e34acd83
istighfarinba <bafarcorp@gmail.com>
2016-04-27 10:43:44 +0700

    deklarasi variabel dan variabel final(harga jaket dan diskon)

--- src/Program Kedua/ProsesPembelian.java
+++ src/Program Kedua/ProsesPembelian.java
@@ -7,8 +7,14 @@ package TokoBaju;
 
 /**
  *
- * @author Bahtiar
+ * @NAMA   :ISTIGHFARIN BAHTIAR AMRY
+ * @NIM    :155150401111129
+ * @KELAS  :SI-H
  */
 public class ProsesPembelian {
+    public final static int hargaA=100000,hargaB=125000,hargaC=175000;
+    public final static int diskonA=95000,diskonB=120000,diskonC=160000;
+    public static int jumA,jumB,jumC;
+    public static int totalA,totalB,totalC;
     
 }
