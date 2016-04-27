895c5b30e833ba698793b106f5d7e6eb0446eb82
istighfarinba <bafarcorp@gmail.com>
2016-04-27 10:17:57 +0700

    tambah method penjumlahan pengurangan(static)

--- src/Program Pertama/hitung.java
+++ src/Program Pertama/hitung.java
@@ -1,11 +1,24 @@
 
-
 /**
  *
  * @NAMA : ISTIGHFARIN BAHTIAR AMRY
- * @NIM  : 155150401111129
+ * @NIM : 155150401111129
  * @KELAS: SI-H
  */
 public class hitung {
-    
+
+    private static int hasil;
+    private static double hasilbagi;
+
+    public static void penjumlahan(int a, int b) {
+
+        hasil = a + b;
+        System.out.println("Maka Hasil Penjumlahan :" + hasil);
+    }
+
+    public static void pengurangan(int a, int b) {
+        hasil = a - b;
+        System.out.println("Maka Hasil pengurangan :" + hasil);
+    }
+
 }
